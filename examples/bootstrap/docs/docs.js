56f90438c8e64cc9989b2d8f74a6fcf2faebaa3d
Jacob Rask <jacob@jacobrask.net>
2012-04-03 22:35:34 +0200

    Update examples

--- examples/bootstrap/docs/docs.js
+++ examples/bootstrap/docs/docs.js
@@ -1,5 +1,6 @@
 // Scans your stylesheet for pseudo classes and adds a class with the same name.
 // Thanks to Knyle Style Sheets for the idea.
+// TODO: Add support for styles inside media queries.
 
 (function() {
 
@@ -18,10 +19,14 @@ var styleSheet = toArray(document.styleSheets)
   .filter(function(ss) {
     return ss.href == null
   })[0];
+
+// Abort if no suiteable stylesheet was found.
+if (!styleSheet) return;
+
 var processedStyles = toArray(styleSheet.cssRules)
   .filter(function(rule) {
-    // Keep only rules with pseudo classes
-    return rule.selectorText.match(pseudoRe);
+    // Keep only rules with pseudo classes.
+    return rule.selectorText && rule.selectorText.match(pseudoRe);
   })
   .map(function(rule) {
     // Replace : with . and encoded :
