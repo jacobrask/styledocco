3cd621e661a02826ef5f4bb01528062b3457f240
Jacob Rask <jacob@jacobrask.net>
2012-06-09 20:46:44 +0200

    Test and boilerplate for new stylesheet parser

--- test/parser.js
+++ test/parser.js
@@ -1,56 +1,28 @@
+var async = require('async');
 var fs = require('fs');
 var path = require('path');
-var langs = require('../lib/languages');
 var parser = require('../lib/parser');
 
-var cssDir = "" + __dirname + "/fixtures/css";
+var cssDir = path.join(__dirname, '/fixtures/css/');
 
-exports["Extract docs+code blocks"] = function(test) {
-  var cssFiles = fs.readdirSync(cssDir).filter(function(filename) {
-    return path.extname(filename) === '.css';
+exports["Extract docs and code blocks"] = function(test) {
+  fs.readdir(cssDir, function(err, files) {
+    if (err != null) throw err;
+    files = files.filter(function(filename) {
+      return path.extname(file === '.css');
+    });
+    async.forEach(files, function(file, cb) {
+      var baseFile = path.join(cssDir, path.basename(file, path.extname(file)));
+      fs.readFile(baseFile + '.css', 'utf-8', function(err, css) {
+        if (err != null) throw err;
+        var extracted = parser(css);
+        fs.readFile(baseFile + '.blocks.json', 'utf-8', function(err, json) {
+          if (err != null) throw err;
+          var saved = JSON.parse(json);
+          test.deepEqual(extracted, saved, "Match failed for " + baseFile);
+          cb();
+        });
+      });
+    }, test.done);
   });
-  var extracted, file, saved;
-  for (var i = 0, len = cssFiles.length; i < len; i++) {
-    file = cssFiles[i];
-    extracted = parser.extractBlocks(
-      langs.getLanguage(file),
-      fs.readFileSync(path.join(cssDir, file), 'utf-8')
-    );
-    saved = JSON.parse(
-      fs.readFileSync(
-        cssDir + "/" + (path.basename(file, path.extname(file))) + ".blocks.json",
-        'utf-8'
-      )
-    );
-    test.deepEqual(extracted, saved, "Match failed for " + cssDir + "/" + file);
-  }
-  test.done();
-};
-
-exports["Get documentation tokens"] = function(test) {
-  var cssFiles = fs.readdirSync(cssDir).filter(function(filename) {
-    return path.extname(filename) === '.css';
-  });
-  var extracted, file, saved;
-  for (var i = 0, len = cssFiles.length; i < len; i++) {
-    file = cssFiles[i];
-    extracted = JSON.parse(JSON.stringify(
-      parser.makeSections(
-        parser.extractBlocks(
-          langs.getLanguage(file),
-          fs.readFileSync(
-            path.join(cssDir, file), 'utf-8'
-          )
-        )
-      )
-    ));
-    saved = JSON.parse(
-      fs.readFileSync(
-        cssDir + "/" + (path.basename(file, path.extname(file))) + ".sections.json",
-        'utf-8'
-      )
-    );
-    test.deepEqual(extracted, saved, "Match failed for " + cssDir + "/" + file);
-  }
-  test.done();
 };
