88a01a53fba27edf9dfdb71ca2d7dd4235d13056
Jacob Rask <jacob@jacobrask.net>
2012-06-09 23:09:39 +0200

    Add test for sections and update fixtures to not use highlighter output

--- test/parser.js
+++ test/parser.js
@@ -11,7 +11,7 @@ if (typeof window === 'undefined') {
 }
 var fixtures = [ 'normal', 'structured' ];
 
-exports["Extract docs and code blocks"] = function(test) {
+exports["Separate docs and code blocks"] = function(test) {
   async.forEach(fixtures, function(fixName, cb) {
     async.parallel({
       css: function(cb2) {
@@ -25,6 +25,28 @@ exports["Extract docs and code blocks"] = function(test) {
       if (err != null) throw err;
       var extracted = parser.separate(res.css);
       var saved = JSON.parse(res.blocks);
+      test.deepEqual(extracted, saved, "Match failed for " + fixName);
+      cb();
+    });
+  }, test.done);
+};
+
+exports["Make sections"] = function(test) {
+  async.forEach(fixtures, function(fixName, cb) {
+    async.parallel({
+      css: function(cb2) {
+        io.readFile(fixturePath + fixName + '.css', cb2);
+      },
+      sections: function(cb2) {
+        io.readFile(fixturePath + fixName + '.sections.json', cb2);
+      }
+    },
+    function(err, res) {
+      if (err != null) throw err;
+      var extracted = JSON.parse(JSON.stringify(parser.makeSections(
+        parser.separate(res.css)
+      )));
+      var saved = JSON.parse(res.sections);
       test.deepEqual(extracted, saved, "Match failed for " + fixName);
       cb();
     });
