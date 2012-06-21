d472f36c8f4d7e86b53979f93e22a8162cd8bde7
Jacob Rask <jacob@jacobrask.net>
2012-06-21 22:29:16 +0200

    Update examples with new file structure and inlined CSS/JS

--- test/paths.js
+++
@@ -1,8 +0,0 @@
- var makeDestination = require('../lib/utils').makeDestination;
-
- exports["Make destination"] = function(test) {
-   test.equal(makeDestination('foo/bar/baz.css'), 'foo-bar-baz.html');
-   test.equal(makeDestination('foo\\bar\\baz.css'), 'foo-bar-baz.html');
-   test.equal(makeDestination('foo.css'), 'foo.html');
-   test.done();
- };
