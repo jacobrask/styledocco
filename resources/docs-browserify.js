3acb7a1d268dc21ec8845973c988312405916853
Jacob Rask <jacob@jacobrask.net>
2012-07-19 10:47:24 +0200

    Use data uri iframes for browsers supporting that
    Only WebKit browsers need to load the current location.href to be able
    to interact with it.

--- resources/docs-browserify.js
+++ resources/docs-browserify.js
@@ -19,9 +19,6 @@ var $body = $('body').first();
 // Loop through code previews and replace with iframes.
 $('.preview').each(function() {
   var $oldPreview = $(this);
-  // Insert a new iframe with the current document as src.
-  // In Chrome this will count as same-origin, in other browsers it will
-  // not load if it's on file systems, but still be script-modifiable.
   var $iframe = $(document.createElement('iframe'))
                   .attr('seamless', 'seamless')
                   .data('code', $oldPreview.html());
@@ -44,19 +41,28 @@ $('.preview').each(function() {
     // Add preview specific scripts and styles. We can't use jQuery methods
     // here due to the way it handles script insertion.
     var scriptEl = doc.createElement('script');
-    scriptEl.src = 'previews.js';
+    var src = location.href.split('/');
+    src.pop(); src.push('previews.js');
+    scriptEl.src = src.join('/');
     var styleEl = doc.createElement('style');
     styleEl.innerHTML = styles;
     var headEl = doc.createElement('head');
-    headEl.appendChild(scriptEl);
     headEl.appendChild(styleEl);
+    headEl.appendChild(scriptEl);
     oldHeadEl.parentNode.replaceChild(headEl, oldHeadEl);
+    $preview.removeClass('loading');
 
     // Set the height of the iframe element to match the content.
-    $preview.removeClass('loading');
-    $iframe.css('height', $body[0].scrollHeight);
+    $iframe.height($body[0].scrollHeight);
   });
-  $iframe.attr('src', location.href + '#preview');
+  if ($.browser.webkit) {
+    // WebKit doesn't treat data uris as same origin [https://bugs.webkit.org/show_bug.cgi?id=17352]
+    // Even with try/catch, errors will be thrown, so there's no good way to feature detect.
+    $iframe.attr('src', location.href + '#preview');
+  } else {
+    // Set source to an empty HTML document.
+    $iframe.attr('src', 'data:text/html,%3C!doctype%20html%3E%3Chtml%3E%3Chead%3E%3Ctitle%3E%3C%2Ftitle%3E%3C%2Fhead%3E%3Cbody%3E');
+  }
 });
 
 // Allow `resize` to shrink in WebKit by setting width/height to 0 when
