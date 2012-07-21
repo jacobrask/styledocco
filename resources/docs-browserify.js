87b846b3e189627d57a05a55566832ce94ecab23
Jacob Rask <jacob@jacobrask.net>
2012-07-21 12:20:22 +0200

    Add a bottom bar with buttons to resize previews
    For responsive web design debugging

--- resources/docs-browserify.js
+++ resources/docs-browserify.js
@@ -110,4 +110,16 @@ $body.on('click', function(event) {
   }
 });
 
+$('.settings').on('click', 'button', function(event) {
+  var $el = $(event.target);
+  $el.addClass('is-active');
+  $el.siblings().removeClass('is-active');
+  $('.resizeable')
+    .animate({ width: $el.data('width') }, 250, function(el) {
+      var $iframe = $(this).find('iframe').first();
+      $iframe.animate({ height: getContentHeight($iframe[0]) }, 100);
+    });
+});
+
+
 }());
