ea03c3f122e2c6c0f7c4dea205a92af822f98661
Jacob Rask <jacob@jacobrask.net>
2012-08-29 14:18:02 +0200

    Make sure tests don't interfere with each other

--- test/browser/iframes.js
+++ test/browser/iframes.js
@@ -4,7 +4,8 @@ var doc = document;
 
 buster.testCase("Iframes", {
   tearDown: function() {
-      doc.body.innerHTML = '';
+    doc.body.innerHTML = '';
+    doc.head.innerHTML = '';
   },
   "Same origin data uri feature detection": function(done) {
     test.sameOriginDataUri(function(err, support) {
@@ -25,26 +26,16 @@ buster.testCase("Iframes", {
       test.createPreview(0, false).src,
       location.href + '#__preview__');
   },
-  "Replace document content": function(done) {
-    // TODO: Try to call replaceDocumentContent on current document
-    // instead of iframe, without breaking other tests
-    test.sameOriginDataUri(function(err, support) {
-      var iframeEl = test.createPreview(0, support);
-      iframeEl.addEventListener('load', function() {
-        var doc = this.contentDocument;
-        test.replaceDocumentContent(doc, {
-          scripts: 'window.TESTING = true',
-          styles: 'body{display:none}',
-          html: 'TESTING'
-        });
-        assert.equals(doc.body.innerHTML, 'TESTING', 'Change body content');
-        assert.equals(
-          doc.defaultView.getComputedStyle(doc.body).getPropertyValue('display'),
-          'none', 'Update document styles');
-        assert(doc.defaultView.TESTING, 'Add and execute JavaScript');
-        done();
-      });
-      doc.body.appendChild(iframeEl);
+  "Replace document content": function() {
+    test.replaceDocumentContent(doc, {
+      scripts: 'window.TESTING = true',
+      styles: 'body{display:none}',
+      html: 'TESTING'
     });
+    assert.equals(doc.body.innerHTML, 'TESTING', 'Change body content');
+    assert.equals(
+      doc.defaultView.getComputedStyle(doc.body).getPropertyValue('display'),
+      'none', 'Update document styles');
+    assert(doc.defaultView.TESTING, 'Add and execute JavaScript');
   }
 });
--- test/browser/preview-height.js
+++ test/browser/preview-height.js
@@ -6,18 +6,15 @@ var doc = document;
 
 buster.testCase("Get document height", {
   setUp: function() {
-    if (!doc.styleSheets.length) {
-      doc.head.appendChild(doc.createElement('style'));
-    }
+    doc.head.innerHTML = '';
+    doc.head.appendChild(doc.createElement('style'));
     this.styleSheet = doc.styleSheets[0];
     this.styleSheet.insertRule('* { margin: 0; padding: 0 }', 0);
-    this.testEl = doc.createElement('div');
-    this.testEl.id = 'content';
-    doc.body.appendChild(this.testEl);
+    doc.body.innerHTML = '<div id="content"></div>';
   },
   tearDown: function() {
-    while (this.styleSheet.cssRules.length) this.styleSheet.deleteRule(0);
-    doc.body.removeChild(this.testEl);
+    doc.head.innerHTML = '';
+    doc.body.innerHTML = '';
   },
   "position: static": function() {
     this.styleSheet.insertRule('#content { height: 200px }', 1);
