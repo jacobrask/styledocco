fe437dd40a1288449928603edeb7dcc76e5e4482
Jacob Rask <jacob@jacobrask.net>
2012-09-03 10:52:58 +0200

    Use getters and setters on textareas

--- share/docs.js
+++ share/docs.js
@@ -18,13 +18,22 @@ _(doc.getElementsByClassName('preview-code')).forEach(function(codeEl) {
 
   renderPreview(codeEl);
 
-  var editEvent = new CustomEvent('edit');
-  var didChange = function() {
-    if (this._oldValue !== this.value) this.dispatchEvent(editEvent);
-    this._oldValue = this.value;
+  codeEl._editEvent = new CustomEvent('edit');
+  codeEl._code = '';
+  Object.defineProperty(codeEl, 'code', {
+    get: function() {
+      return this._code;
+    },
+    set: function(val) {
+      this.dispatchEvent(this._editEvent);
+      this._code = val;
+    }
+  });
+  var updateCode = function() {
+    if (this.code !== this.value) this.code = this.value;
   };
-  codeEl.addEventListener('keypress', didChange);
-  codeEl.addEventListener('keyup', didChange);
+  codeEl.addEventListener('keypress', updateCode);
+  codeEl.addEventListener('keyup', updateCode);
 
 });
 
