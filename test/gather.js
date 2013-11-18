0fe6422803d35334031e42a4b6ee9bbe110bfcd1
David Marrs <david@marrsbytes.com>
2013-11-18 00:43:42 +0000

    Add tests to cover gathering of input files.

---
+++ test/gather.js
@@ -0,0 +1,40 @@
+var gather = require('../src/gather')
+  , expect = require('chai').expect
+  ;
+
+function this_dir(s) {
+  return __dirname + '/' + (s || '');
+}
+
+// Allow non-mocha frameworks to skip over these tests.
+if (!global.describe) { process.exit(); }
+
+describe('gather', function() {
+  // Contains functions used for gathering all of the files that will
+  // be used by Styledocco as it generates the documentation.
+  
+  it('returns a method for gathering input files', function() {
+    expect(gather().files).to.exist;
+  });
+
+  describe('files', function() {
+    it('Ignores hidden files', function(done) {
+      gather({"in": [this_dir('fixtures/input-files')]}).files(function(err, files) {
+        for (var i in files) {
+          expect(files[i]).to.not.match(/^\./);
+        }
+        done();
+      });
+    });
+
+    it('has a file extension matching the allowed file types', function(done) {
+      gather({"in": [this_dir('fixtures/input-files')]}).files(function(err, files) {
+        for (var i in files) {
+          // Expect css, scss, sass, less, or styl.
+          expect(files[i]).to.match(/.*\.(s?[ca]ss|less|styl)$/);
+        }
+        done();
+      });
+    });
+  });
+});
